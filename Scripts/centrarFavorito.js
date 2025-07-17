b4e24ffa83f1e2f8e5ac627fddfffa205003140a
Charlydob <carloseduardooliveirabarbosa@gmail.com>
2025-07-17 07:17:19 +0200

    Arreglo maps modular 17

--- Scripts/centrarFavorito.js
+++ Scripts/centrarFavorito.js
@@ -5,6 +5,7 @@ import { map, userMarker, currentCoords } from "./variablesGlobales.js"; // ✅
 import { actualizarCirculo } from "./circuloBusqueda.js"; // ✅
 import { actualizarBusquedaActiva } from "./searchManager.js"; // ✅
 
+
 export function getLocation() {
   if (!navigator.geolocation) {
     alert("Tu navegador no permite geolocalización");
@@ -15,8 +16,8 @@ export function getLocation() {
   navigator.geolocation.getCurrentPosition(
     (pos) => {
       const lat = pos.coords.latitude;
-      const lon = pos.coords.longitude;
-      initMap(lat, lon);
+      const lng = pos.coords.longitude;
+      initMap(lat, lng);
     },
     () => {
       console.warn("No se pudo obtener la ubicación. Usando ubicación por defecto.");
